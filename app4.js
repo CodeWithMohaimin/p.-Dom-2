4280d0c6ea73aabb315bd92f0f397a836835b20b
programmermohaimin <you@example.com>
2022-06-25 16:11:35 +0600

    app4

---
+++ app4.js
@@ -0,0 +1,62 @@
+
+// let li = document.createElement('li')
+// li.innerHTML = 'Four';
+
+// let list = document.getElementById('list')
+// list.appendChild(li)
+
+let li = createElement('li','', 'four')
+let list = document.getElementById('list')
+// list.appendChild(li)
+
+
+// let li2 = createElement('li', '', 'Five')
+// list.appendChild(li2)
+
+
+
+let p = createElement('p', 'lead', 'I am a programmer mohaimin and I am learning web development . Still now I am a Front-end Developer, But in future I wanted to become e Full-Stack Developer. I have own plan and I create a Company near future. I will help you to make your dream on your project.');
+
+let p2 = createElement('p', 'lead', 'I am a programmer mohaimin and I am learning web development . Still now I am a Front-end Developer, But in future I wanted to become e Full-Stack Developer. I have own plan and I create a Company near future. I will help you to make your dream on your project.');
+
+
+let p3 = createElement('p', 'lead', 'I am a programmer mohaimin and I am learning web development . Still now I am a Front-end Developer, But in future I wanted to become e Full-Stack Developer. I have own plan and I create a Company near future. I will help you to make your dream on your project.');
+
+
+let p4 = createElement('p', 'lead', 'I am a programmer mohaimin and I am learning web development . Still now I am a Front-end Developer, But in future I wanted to become e Full-Stack Developer. I have own plan and I create a Company near future. I will help you to make your dream on your project.');
+
+let div = createElement('div')
+append(div,[p,p2,p3,p4])
+// div.appendChild(p)
+// div.appendChild(p2)
+
+
+// let container = document.getElementById('cont')
+// container.appendChild(div)
+
+
+// let container = document.getElementById('cont')
+// append(cont,[p,p2,p3,p4])
+
+list.insertAdjacentElement('beforebegin',div)
+list.style.background = '#f45'
+
+
+
+
+
+
+
+
+//create append function
+function append(parent, children) {
+    children.forEach(child => parent.appendChild(child))
+}
+// custom create element function
+function createElement(tagname, className ,innerHTML) {
+    let tag = document.createElement(tagname)
+    tag.className = className ||'';
+    tag.innerHTML = innerHTML ||'';
+
+    return tag
+}
